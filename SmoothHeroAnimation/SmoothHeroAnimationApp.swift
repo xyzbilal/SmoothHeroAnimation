81baf05f3ab2e734d0da509aa8ac051947f6aeea
bilal simsek <xyzbilal@gmail.com>
2023-07-25 15:21:07 +0300

    Initial Commit

---
+++ SmoothHeroAnimation/SmoothHeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothHeroAnimationApp.swift
+//  SmoothHeroAnimation
+//
+//  Created by Bilal SIMSEK on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmoothHeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
